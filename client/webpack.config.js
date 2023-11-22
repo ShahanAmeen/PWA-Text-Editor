04d424a7eceec56491602b220274d268e3acb119
Shahan Ameen <Shahanameen@gmail.com>
2023-11-22 16:31:55 -0600

    added rule for css and babel loader for webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -113,6 +113,22 @@ module.exports = () => {
           When you copy and paste them below, you'll be done here.
         */
 
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
